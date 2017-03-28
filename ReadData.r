844dbb50b075fea6ce3b6f2492add89c9bf13fb0
Rick Qui <rick.qui@netscout.com>
2017-03-28 23:48:24 +0100

    update read data to R

--- ReadData.r
+++ ReadData.r
@@ -132,7 +132,15 @@ placeNameLink <- ldply(placeNameLink)
 head(placeNameLink)
 tail(placeNameLink)
 
+download.file("http://www.jaredlander.com/data/SocialComments.xml", destfile ="data/tmp.xml")
 
-
-
-
+teaFile <- "http://www.jaredlander.com/data/SocialComments.xml"
+require(XML)
+teaParsed  <- xmlToList(teaFile)
+length(teaParsed)
+str(teaParsed)
+teaParsed[[1]][[1]]$id
+teaParsed[[1]][[1]]$author$name
+teaParsed[[1]][[1]]$published
+teaParsed[[1]][[1]]$content$.attrs
+teaParsed[[1]][[1]]$content$.attrs[["sentimentScore"]]
