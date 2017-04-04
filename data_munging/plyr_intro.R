9ef60fd9c154ae7c9594d27ac4e69e76399932aa
Rick Qui <rick.qui@netscout.com>
2017-04-04 06:19:09 +0100

    add plyr intro

---
+++ data_munging/plyr_intro.R
@@ -0,0 +1,17 @@
+theList <- list(A=matrix(1:9, 3), B=1:5, C=matrix(1:4, 2), D=2)
+theList
+
+lapply(theList, sum)
+llply(theList, sum)
+identical(lapply(theList, sum), llply(theList, sum))
+
+sapply(theList, sum)
+laply(theList, sum)
+
+head(diamonds)
+aggregate(price ~ cut, diamonds, each(mean, median))
+
+colwise
+
+numcolwise(sum, na.rm=TRUE)(diamonds)
+sapply(diamonds[, sapply(diamonds, is.numeric)], sum)
